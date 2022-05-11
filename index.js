d1570705868034cace48249fa62d02573d4e906d
amelia <amelia@amyy.cc>
2022-05-11 08:31:14 -0500

    peer to pee fully working & added exe

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ const { app, BrowserWindow } = require('electron')
 const path = require('path')
 const createWindow = () => {
     const win = new BrowserWindow({
-      width: 800,
-      height: 600,
+      
       autoHideMenuBar: true,
       icon: __dirname + '/icon.png',
       frame: false
     })
-  
+    win.maximize()
     win.loadFile('index.html')
    
   }
   app.whenReady().then(() => {
     createWindow()
+    
   })
+
+  //npm exec electron-packager .
---
+++ install.bat
@@ -0,0 +1,1 @@
+start npm exec electron-packager .
