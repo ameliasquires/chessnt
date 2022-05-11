95bdc149ce081809140bbb17500c2b64cb0b5cc3
amelia <amelia@amyy.cc>
2022-05-11 11:54:13 -0500

    updated install.bat to create a shortcut

--- install.bat
+++ install.bat
@@ -1,1 +1,5 @@
-start npm exec electron-packager .
+@echo off
+echo installing for you senpai ~nya
+npm exec electron-packager . & start chessnt-win32-x64/chessnt.exe & set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs" & echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT% & echo sLinkFile = "%CD%\start.lnk" >> %SCRIPT% & echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT% & echo oLink.TargetPath = "%CD%\chessnt-win32-x64\chessnt.exe" >> %SCRIPT% & echo oLink.Save >> %SCRIPT% & cscript /nologo %SCRIPT% & del %SCRIPT%
+
+
--- readme.md
+++ readme.md
@@ -2,5 +2,9 @@
 
 [view it here](https://squiresgrant.github.io/chessnt/)
 
+#### prerequisites:
+-node.js
+thats all i think
+
 #### todo:
 -finish readme file
